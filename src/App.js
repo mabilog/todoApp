b3370e630df1ae66854cd74e22233c9e6e66693a
Mabilog <g.angelo.m@gmail.com>
2021-07-03 10:18:40 -0400

    Todo list done with React and Firebase

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         start: firebase.firestore.FieldValue.serverTimestamp(),
         todo: todoInput
       })
-      setTodoInput('');
+      setTodoInput("");
   }
 
   const getTodos = () => {
@@ -39,28 +39,38 @@ function App() {
         }))) 
       }) // when new data added, instantly reflected on our list
   }
-  
+
 
   return (
     <AppWrapper>
-      <Title>Angelo's Todo App Powered by ⚛React and 🔥Firebase! </Title>
+      <Title>Angelo's Todo App </Title>
+      <Subtitle>Powered by ⚛React and 🔥Firebase!</Subtitle>
       <AddWrapper>
         <TextField 
         id="standard-basic" 
-        label="Todo" 
-        style={{width: '100%', maxWidth: '300px'}}
-        onChange={(e) => {
-          setTodoInput(e.target.value)
-        }}
+        label="Write a todo" 
+        value={todoInput}
+        style={{width: '100%', maxWidth: '400px'}}
+        onChange={(e) => setTodoInput(e.target.value)}
+        
       />
       <Button 
         variant='contained'
         color="primary" 
         onClick={addTodo}
         type="submit"
-      >Add</Button>
+      >add</Button>
       </AddWrapper>
-      {todos.map(todo => <Todo todo={todo}/>)}
+      <TodoWrapper>
+        {todos.map(todo => (
+        <Todo 
+          id={todo.id}
+          inprogress={todo.inprogress}
+          todo={todo.todo}
+          start={todo.start}
+          />))}
+      </TodoWrapper>
+      
     </AppWrapper>
   );
 }
@@ -70,7 +80,7 @@ const AppWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center
+  text-align: center;
 `;
 
 
@@ -80,10 +90,23 @@ const Title = styled.h1`
   max-width: 400px;
 `;
 
+const Subtitle = styled.p`
+  font-size: 16px;
+  width: 100%;
+  max-width: 400px;
+`;
+
 const AddWrapper = styled.form`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  
+`;
+
+const TodoWrapper = styled.ul`
+  text-decoration: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  max-width: 350px;
 `;
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Checkbox, ListItem, ListItemText } from '@material-ui/core';
+import { db } from './firebase_config';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const Todo = ({todo}) => {
+const Todo = ({inprogress, todo, start, id}) => {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = (e) => {
+    setChecked(e.target.checked);
+  }
+
+  const toggleProgress = () => {
+    db.collection('todos')
+      .doc(id)
+      .update({
+        inprogress: !inprogress})
+  }
+
+  const deleteTodo = () => {
+    db.collection('todos')
+      .doc(id)
+      .delete()
+  }
+  
   return (
-    <div key={todo.id}>
-      {todo.todo}
-    </div>
+    <ListItem key={id}>
+      <ListItemText primary={`${todo}`} secondary={`${inprogress ? 'in progress ⏳' : 'completed ✔'}`} />
+      {/* <Button variant="contained" onClick={toggleProgress}>{inprogress ? 'Done' : 'unDone'}</Button> */}
+      <Checkbox
+        checked={checked}
+        color="primary"
+        onChange={handleChange}
+        onClick={toggleProgress}
+        />
+      <Button onClick={deleteTodo}><DeleteIcon/></Button>
+    </ListItem>
   )
 }
+
 
 export default Todo
